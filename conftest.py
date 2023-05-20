0e2ad1e408fef5d07aadd54a43274f2e94af6eb8
Chekanov Artem <tema-42@mail.ru>
2023-05-20 23:15:06 +0700

    add dotenv

--- conftest.py
+++ conftest.py
@@ -1,8 +1,11 @@
+import os
+
 import pytest
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selene import Browser, Config
 from utils import attach
+from dotenv import load_dotenv
 
 DEFAULT_BROWSER_VERSION = '100.0'
 def pytest_addoption(parser):
@@ -10,6 +13,10 @@ def pytest_addoption(parser):
         '--browser_version',
         default='100.0'
     )
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function')
 def setup_browser(request):
@@ -26,8 +33,10 @@ def setup_browser(request):
     }
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
     browser = Browser(Config(driver))
 
